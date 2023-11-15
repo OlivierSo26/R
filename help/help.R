f7b375146aedbb1db16493e19edacc7447d86b19
OlivierSO <olivier.smeja@etu.univ-lyon1.fr>
2023-11-15 15:02:13 +0100

    ajout sous-dossier doc

---
+++ help/help.R
@@ -0,0 +1,7 @@
+<!-- * cf ```{r} help("text")```   -->
+    <!-- * cf ```{r} help(text)```  -->
+    <!-- * cf `{r} help(text)` -->
+    * cf `r ?(text)` Donne le chemin du fichier AIDE sur mon PC
+help(help)
+http://127.0.0.1:22796/library/graphics/html/text.html 
+https://rdocumentation.org/packages/graphics/versions/3.6.2/topics/text         
